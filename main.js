cd7c2025bb8bc3a46f11c0fc3c27f0ce991d98e2
umarodev <ohojimirzayev@gmail.com>
2022-04-23 11:52:10 +0300

    Add xIcon for navbar

--- main.js
+++ main.js
@@ -2,7 +2,18 @@
 
 const hamburgerIcon = document.querySelector('.nav-toggle');
 const navbarItems = document.querySelector('.nav-items');
+const xIcon = document.querySelector('.x-icon');
 
 hamburgerIcon.addEventListener('click', function(){
-   navbarItems.classList.toggle('navbar-active');
+   navbarItems.classList.add('navbar-active');
+   hamburgerIcon.classList.add('for-rotate');
+   hamburgerIcon.style.display = "none";
+   xIcon.style.display = "inline-block";
+})
+
+xIcon.addEventListener('click', function(){
+   navbarItems.classList.remove('navbar-active');
+   xIcon.classList.add('for-rotate');
+   xIcon.style.display = "none";
+   hamburgerIcon.style.display = "inline-block"
 })
